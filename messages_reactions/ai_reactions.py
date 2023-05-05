52136ff88db019655686b361fb4457cff146002c
Diogo Vechio <diogo.rva@gmail.com>
2023-05-05 09:22:10 -0300

    asd

--- messages_reactions/ai_reactions.py
+++ messages_reactions/ai_reactions.py
@@ -404,14 +404,17 @@ async def openai_reactions(
             bot.mocked_hour = bot.datetime_now.hour
 
             with bot.sending_action(message.chat.id, action="typing"):
-                chat = "\n".join(bot.messages_in_memory[message.chat.id][-20:])
+                chat = "\n".join(bot.messages_in_memory[message.chat.id][-25:])
                 bot.loop.create_task(
                     bot.send_message(
                         message_text=await bot.openai.generate_message(
                             message_username='.',
                             message_text=chat,
                             chat=message.chat.title,
-                            prompt_inject="continue a conversa a seguir: ",
+                            prompt_inject='considere que você é o "pedro", abaixo é uma conversa entre você e '
+                                          'seus amigos, comente algum dos assuntos criando uma curta resposta '
+                                          'para "pedro" no final: ',
+                            only_chatgpt=True,
                             biased=False,
                         ),
                         chat_id=message.chat.id,
