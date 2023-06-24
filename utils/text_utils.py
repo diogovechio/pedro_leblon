2721e9936ac29bcc19ac153c71997992deaa83ea
Diogo Vechio <diogo.rva@gmail.com>
2023-06-24 09:56:03 -0300

    asd

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -184,7 +184,7 @@ async def normalize_openai_text(
 
                 if letter in (".", "!", "?", ":"):
                     idx_to_lower = i + 2
-                elif letter == '"':
+                elif letter in ('"', "\n"):
                     idx_to_lower = i + 1
         else:
             ai_message = original_message
