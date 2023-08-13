0699ab0b73b22515fc60eed9c6cde8ff5fd2a7e8
Diogo Vechio <diogo.rva@gmail.com>
2023-08-13 18:35:08 -0300

    asd

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import typing as T
+from random import random
 
 import aiohttp
 import asyncio
@@ -25,6 +26,7 @@ async def telegram_logging(text: T.Union[str, Exception], chat_id=-704277411):
     api_route = f"https://api.telegram.org/bot{TOKEN}"
 
     async with asyncio.Semaphore(5):
+        await asyncio.sleep(5 + 10 * random())
         await session.post(
                 f"{api_route}/sendMessage".replace('\n', ''),
                 json={
